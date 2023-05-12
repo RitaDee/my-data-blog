acc3aa63741ea6d754be82b11ebd564cecea9ddb
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-12 12:42:02 +0100

    Update irb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,4 @@
-# class Comment < ApplicationRecord
-#   # belongs_to :user
-#   # belongs_to :post
-#   belongs_to :author, foreign_key: :user_id, class_name: 'User'
-#   belongs_to :post, class_name: 'Post', counter_cache: :commentscounter
-# end
+class Comment < ApplicationRecord
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  belongs_to :post, class_name: 'Post', counter_cache: :comments_counter
+end
