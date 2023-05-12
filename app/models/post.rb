69ac2d9acbb96db2accc9bf8d4b77792062af329
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-12 14:59:18 +0100

    Add Rails Active Callbacks - after_save to call the #update_user_post_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  after_save :update_user_post_counter
+
+  private
+
   def update_user_post_counter
     author.increment!(:postscounter)
   end
