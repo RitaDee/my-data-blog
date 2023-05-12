863876bd98fbb5266809a671bf072d687149b2ba
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-12 15:05:06 +0100

    Add Rails Active Callbacks - after_save to call the #update_user_post_like_counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
   belongs_to :post, counter_cache: :likescounter
 
+  after_save :update_post_like_counter
+
+  private
+
   def update_post_like_counter
     post.increment!(:likescounter)
   end
