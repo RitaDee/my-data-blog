8f369e672fa1f9025841a554fbdf1d23deb2fbf1
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-16 21:26:39 +0100

    Modify PostsController class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @users = User.find(params[:id])
-    @posts = @users.posts
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
