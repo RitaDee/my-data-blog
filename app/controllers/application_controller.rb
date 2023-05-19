e2fd5664e45c69e76d23cc83ae2bbde2ec085bba
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-19 16:51:44 +0100

    Provide protection against CSRF attacks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :udpate_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def udpate_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+    end
+  end
 end
