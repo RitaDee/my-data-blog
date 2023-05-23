3904b5d1a8dc0cc549d4429615e486d33f5686e8
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-23 13:34:09 +0100

    Add integration test to post index

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -2,20 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'User Post Index Page', type: :feature do
   before do
-    # Create necessary objects for the tests, such as a user, posts, and comments
-    @user = User.create(name: 'Rita', photo: 'profile.jpg')
-    @user.save # Save the user object
-    @first_post = Post.create(title: 'Post 1', text: 'Lorem ipsum dolor sit amet.', author: @user)
-    @second_post = Post.create(title: 'Post 2', text: 'Consectetur adipiscing elit.', author: @user)
-    # Create comments for the posts
-    Comment.create(text: 'First comment on post 1', post: @post1)
-    Comment.create(text: 'Second comment on post 1', post: @post1)
-    Comment.create(text: 'Comment on post 2', post: @post2)
+    @user = User.create(name: 'Rita', photo: 'profile.jpg', bio: 'Teacher from Brazil.', posts_counter: 0)
+    @user.save
+
+    @first_post = Post.create(title: 'Post 1', text: 'Lorem ipsum dolor sit amet.', comments_counter: 0, likes_counter: 0, author_id: @user.id)
+    @second_post = Post.create(title: 'Post 2', text: 'Consectetur adipiscing elit.', comments_counter: 0, likes_counter: 0, author_id: @user.id)
+
+    Comment.create(text: 'First comment on post 1', author_id: @user.id, post_id: @first_post.id)
+    Comment.create(text: 'Second comment on post 1', author_id: @user.id, post_id: @first_post.id)
+    Comment.create(text: 'Comment on post 2', author_id: @user.id, post_id: @second_post.id)
   end
 
   scenario 'Display user post index page' do
-    visit user_posts_path(@user.id)
-
+    visit user_posts_path(@user)
 
     # Test: Verify user's profile picture is displayed
     expect(page).to have_css("img[src*='profile.jpg']")
@@ -26,26 +25,23 @@ RSpec.feature 'User Post Index Page', type: :feature do
     # Test: Verify number of posts the user has written is displayed
     expect(page).to have_content('Number of posts')
 
-    # Test: Verify post titles and bodies are displayed
-    expect(page).to have_content('Post 1')
-    expect(page).to have_content('Lorem ipsum dolor sit amet.')
-    expect(page).to have_content('Post 2')
-    expect(page).to have_content('Consectetur adipiscing elit.')
+    # Test: Verify post titles
+    expect(page).to have_content(@first_post.title)
+    expect(page).to have_content(@second_post.title)
 
-    # Test: Verify first comments on posts are displayed
+    # Test: Verify first comments on posts body are displayed
     expect(page).to have_content('First comment on post 1')
     expect(page).to have_content('Second comment on post 1')
     expect(page).to have_content('Comment on post 2')
 
     # Test: Verify number of comments and likes on posts are displayed
-    expect(page).to have_content('2 comments')
-    expect(page).to have_content('0 likes')
+    expect(page).to have_content("Likes: #{@first_post.likes_counter}")
 
     # Test: Verify pagination section is displayed
-    expect(page).to have_selector('.pagination')
+    expect(page).to have_button('Pagination')
 
     # Test: Click on a post and verify redirection to post show page
-    click_link 'Post 1'
-    expect(page).to have_current_path(user_post_path(@user, @post1))
+    click_link('a', match: :first)
+    expect(page).to have_content(@first_post.title)
   end
 end
