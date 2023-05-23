4f80e4d382cc9a14bfb1b2413e2459a08fe5a074
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-23 11:17:53 +0100

    Modify user_show_spec.rb file

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -3,33 +3,34 @@ require 'rails_helper'
 RSpec.feature 'User Show Page', type: :feature do
   before do
     # Create necessary objects for the tests, such as a user and their posts
-    @user = User.create(name: 'John', photo: 'profile.jpg', bio: 'Software Engineer', posts_counter: 0)
-    @post1 = Post.create(author: @user, title: 'Post 1', body: 'Lorem ipsum dolor sit amet.')
-    @post2 = Post.create(author: @user, title: 'Post 2', body: 'Consectetur adipiscing elit.')
-    @post3 = Post.create(author: @user, title: 'Post 3', body: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+    @user = User.create(name: 'Rita', photo: 'profile.jpg', bio: 'Software Engineer', posts_counter: 6)
+    Post.create(author: @user, title: 'Hi people', text: 'Lorem ipsum dolor sit amet.')
+    Post.create(author: @user, title: 'Good', text: 'Consectetur adipiscing elit.')
+    Post.create(author: @user, title: 'Dev', text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+    @post = Post.create(author: @user, title: 'Hello', text: 'Lol')
   end
 
   scenario 'Display user information' do
-    visit user_path(@user)
+    visit user_path(@user.id)
 
     # Test: Display user's profile picture
     expect(page).to have_css("img[src*='profile.jpg']")
 
     # Test: Display user's username
-    expect(page).to have_content('John')
+    expect(page).to have_content('Rita')
 
     # Test: Display number of posts the user has written
-    expect(page).to have_content('Number of posts: 3')
+    expect(page).to have_content('Number of posts: 6')
 
     # Test: Display user's bio
     expect(page).to have_content('Software Engineer')
 
     # Test: Display the user's first 3 posts
-    expect(page).to have_content('Post 1')
-    expect(page).to have_content('Post 2')
-    expect(page).to have_content('Post 3')
+    expect(page).to have_content(@user.most_three_recent_post[0])
+    expect(page).to have_content(@user.most_three_recent_post[1])
+    expect(page).to have_content(@user.most_three_recent_post[2])
+    expect(page).to have_no_content(Post.where(author: @user))
   end
-
   scenario 'View all posts button redirects to user posts index page' do
     visit user_path(@user)
 
@@ -42,8 +43,8 @@ RSpec.feature 'User Show Page', type: :feature do
     visit user_path(@user)
 
     # Test: Clicking on a user's post redirects to the post's show page
-    click_link 'Post 1'
-    expect(page).to have_current_path(post_path(@post1))
+    click_link('See all posts')
+    expect(page).to have_current_path(user_posts_path(@user))
   end
 
   scenario 'Back button redirects to previous page' do
@@ -51,7 +52,6 @@ RSpec.feature 'User Show Page', type: :feature do
 
     # Test: Clicking "Back" button redirects to the previous page
     click_link 'Back'
-    expect(page).to have_current_path(some_previous_page_path)
-    # Replace `some_previous_page_path` with the appropriate path or expectation for your case
+    expect(page).to have_current_path(user_path(@user))
   end
 end
