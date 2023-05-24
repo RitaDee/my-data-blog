e5f19eed6465d159a756dca73cce1b82e730893a
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-05-24 14:58:01 +0100

    Add method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
+  protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up) do |u|
-  #     u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
-  #   end
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+    end
+  end
 end
