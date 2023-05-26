80e7a2df1029f6700a2ce9bc6b7f9390bcd18101
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-26 21:14:31 +0100

    Updfate user model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  attribute :posts_counter, default: 0
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: :author_id, dependent: :destroy
@@ -8,7 +11,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
   validates :name, presence: true
-  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_three_recent_post
     posts.order(created_at: :desc).limit(3)
